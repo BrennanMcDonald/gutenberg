afa22e903d121f7262a55b4ba391424b9ab338fb
Brennan McDonald <brennan.mcdonald.mail@gmail.com>
2020-07-13 19:12:19 -0400

    Fixed drop shadow.

--- components/File.vue
+++ components/File.vue
@@ -1,11 +1,29 @@
 <template>
+<div>
+      <template v-for="(Item, Index) in MenuButtons">
+        <button
+          class="h-full w-10"
+          @click="Item.command"
+          v-bind:key="Index"
+          v-show="md_toggle"
+          v-if="Item.hasOwnProperty('icon') || Item.hasOwnProperty('text')">
+          <fa v-show="Item.icon" :icon="Item.icon" />
+          <span v-show="Item.text">
+            <b>{{Item.text}}</b>
+          </span>
+        </button>
+        <dropdown v-show="md_toggle" class="px-2" v-bind:key="Index" :options="Item" v-else />
+      </template>
   <wysiwyg :value="file_markdown" @input="fileChanged"  />
+
+</div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 import { mapMutations } from 'vuex'
 import WYSIWYG from '~/components/WYSIWYG/index.vue'
+import MenuButtons from './WYSIWYG/_menuButtons'
 
 export default Vue.extend({
   props: ['owner', 'repo', 'path', 'editor_key'],
@@ -28,6 +46,9 @@ export default Vue.extend({
         return "";
       }
     },
+    MenuButtons() {
+      return MenuButtons;
+    }
   },
 })
 </script>
