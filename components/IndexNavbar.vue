cae6a5d5555748567f0af823f46fea7ed505eb58
Brennan McDonald <brennan.mcdonald.mail@gmail.com>
2020-07-25 15:12:55 -0400

    More header changes.

--- components/IndexNavbar.vue
+++ components/IndexNavbar.vue
@@ -79,8 +79,8 @@
         </div>
         <div>
           <a
-            href="#"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0"
+            @click="() => { this.$auth.logout() }"
+            class="inline-block text-sm px-4 py-2 leading-none border rounded text-black border-black hover:border-transparent hover:text-white hover:bg-gray-600 cursor-pointer mt-4 lg:mt-0"
           >Sign Out</a>
         </div>
       </div>
